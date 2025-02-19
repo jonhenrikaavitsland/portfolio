c59d8da728ecd894286e9782c68eacdb9729a48a
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-20 00:47:57 +0100

    adding to config

--- vite.config.cjs
+++ vite.config.cjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.cjs', '.mjs', '.ts', '.tsx'],
   },
   build: {
     logLevel: 'info',

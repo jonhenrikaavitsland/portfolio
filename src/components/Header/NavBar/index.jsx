eb0e0014c9aede6cc8639534e756b417a2e43d94
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-24 18:25:25 +0100

    hidden before breakpoint md

--- src/components/Header/NavBar/index.jsx
+++ src/components/Header/NavBar/index.jsx
@@ -3,7 +3,7 @@ import NavLinkElement from './NavLinkElement';
 export default function NavBar() {
   return (
     <nav>
-      <ul className='flex gap-10'>
+      <ul className='hidden md:flex gap-10'>
         <NavLinkElement>projects</NavLinkElement>
         <NavLinkElement>skills</NavLinkElement>
         <NavLinkElement>contact</NavLinkElement>

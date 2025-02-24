ac0b5e76c59c643c3d373b13f5bdffbbb9fd7f31
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-24 19:32:29 +0100

    fixed an issue in the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -17,7 +17,7 @@ import NavBar from './NavBar';
  */
 export default function Header() {
   return (
-    <header className='flex justify-between p-5 lg:container md:mx-auto'>
+    <header className='flex w-full p-5 lg:container md:mx-auto'>
       <Logo />
       <NavBar />
     </header>

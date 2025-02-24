e3be9c42a056adb68fd481af9c6a743b9bf60d81
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-24 17:39:18 +0100

    logo icon component

---
+++ src/components/Header/Logo/LogoIcon/index.jsx
@@ -0,0 +1,20 @@
+/**
+ * Renders the logo icon for the application.
+ *
+ * This component displays a circular black background with a centered image, representing a capital "J".
+ * The image is styled to maintain a consistent size, and the layout is responsive, centering the icon on smaller screens.
+ *
+ * @component
+ * @example
+ * // Example usage:
+ * <LogoIcon />
+ *
+ * @returns {JSX.Element} The rendered logo icon.
+ */
+export default function LogoIcon() {
+  return (
+    <div className='bg-black w-10 aspect-square flex justify-center items-center rounded-full mx-auto lg:mx-0'>
+      <img src='/j-sharp-solid.svg' alt='capital letter j on dark background' className='w-[19px]' />
+    </div>
+  );
+}

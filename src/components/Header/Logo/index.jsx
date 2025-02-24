e58c24850fa142931a31ac2733cda88d32b2f99a
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-24 17:40:10 +0100

    logo icon component

---
+++ src/components/Header/Logo/index.jsx
@@ -0,0 +1,28 @@
+import LogoIcon from './LogoIcon';
+
+/**
+ * Renders the logo for the application, combining an icon with descriptive text.
+ *
+ * This component displays a logo icon (via the `LogoIcon` component) alongside text that describes the brand.
+ * The text is styled with different casing and font sizes for responsiveness:
+ * - The first part ("SEO") is rendered in uppercase.
+ * - The ampersand (" & ") is rendered in standard case.
+ * - The remaining text ("digital marketing services") is capitalized.
+ *
+ * The layout adjusts for different screen sizes by stacking the elements in a column on smaller screens and in a row on extra-large screens.
+ *
+ * @component
+ * @example
+ * // Example usage:
+ * <Logo />
+ *
+ * @returns {JSX.Element} The rendered logo component.
+ */
+export default function Logo() {
+  return (
+    <div className='flex flex-col xl:flex-row gap-2.5 xl:gap-5 items-center'>
+      <LogoIcon />
+      <span className='font-semibold text-xl xl:text-2.5xl text-center xl:text-start capitalize'>frontend developer portfolio</span>
+    </div>
+  );
+}

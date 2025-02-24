f6b7a70e486fe3a325a6820cd823d166cdeb34ce
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-24 19:27:55 +0100

    tag component

---
+++ src/components/BioCard/Tag/index.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+export default function Tag({ children }) {
+  return <div className='text-xs leading-4 py-0.5 px-1 rounded-xs bg-gray-400 text-black'>{children}</div>;
+}

4b53e736be3f0bc931a16f2362f9baf65d2e499a
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-26 22:08:39 +0100

    linking my buttons and now they are anchors

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,12 +8,16 @@ export default function Contact() {
             <span className='capitalize'>interested</span> in collaborating? <span className='capitalize'>drop</span> me a message!
           </p>
         </section>
-        <div className='flex-1 flex flex-col gap-5 mx-auto md:mx-0'>
+        <div className='flex-1 flex md:flex-col gap-5 mx-auto md:mx-0'>
           <div>
-            <button className='p-3 rounded-lg capitalize w-40 bg-black hover:bg-black/60 text-white font-semibold cursor-pointer'>linkedIn</button>
+            <a href='https://www.linkedin.com/in/jon-henrik-aavitsland-abaa872b7/' target='blank' className='block text-center p-3 rounded-lg capitalize w-40 bg-black hover:bg-black/60 text-white font-semibold cursor-pointer'>
+              linkedIn
+            </a>
           </div>
           <div>
-            <button className='p-2.75 rounded-lg capitalize w-40 bg-white hover:bg-black/20 border border-black font-semibold cursor-pointer'>github</button>
+            <a href='https://github.com/jonhenrikaavitsland' target='blank' className='block text-center p-2.75 rounded-lg capitalize w-40 bg-white hover:bg-black/20 border border-black font-semibold cursor-pointer'>
+              github
+            </a>
           </div>
         </div>
       </div>

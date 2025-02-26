0f1a15c0d353134c815b094773fa96fa3018901c
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-26 22:04:36 +0100

    contact component

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,22 @@
+export default function Contact() {
+  return (
+    <div className='border-b border-black/20'>
+      <div className='lg:container flex flex-col md:flex-row gap-10 md:gap-0 mx-auto px-5 py-15 '>
+        <section className='flex-1 flex flex-col gap-6 md:justify-center text-center md:text-start'>
+          <h2 className='font-bold text-xl md:text-[2.5rem]/12 capitalize'>get in touch</h2>
+          <p>
+            <span className='capitalize'>interested</span> in collaborating? <span className='capitalize'>drop</span> me a message!
+          </p>
+        </section>
+        <div className='flex-1 flex flex-col gap-5 mx-auto md:mx-0'>
+          <div>
+            <button className='p-3 rounded-lg capitalize w-40 bg-black hover:bg-black/60 text-white font-semibold cursor-pointer'>linkedIn</button>
+          </div>
+          <div>
+            <button className='p-2.75 rounded-lg capitalize w-40 bg-white hover:bg-black/20 border border-black font-semibold cursor-pointer'>github</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

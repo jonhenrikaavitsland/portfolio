b81c858e12d2f14021a25903fb63e3b0967981ce
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-02-26 22:23:24 +0100

    footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className='capitalize text-center text-xl/7 py-15'>
+      <p>
+        © 2025{' '}
+        <a href='https://www.linkedin.com/in/jon-henrik-aavitsland-abaa872b7/' target='blank' className='text-blue-950 font-semibold underline underline-offset-4'>
+          jon henrik åvitsland
+        </a>
+        . all rights reserved
+      </p>
+    </footer>
+  );
+}
